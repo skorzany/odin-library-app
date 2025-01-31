92304f16cb28a243c2e28ec5aa130378120c1d0d
skorzany <177423232+skorzany@users.noreply.github.com>
2025-01-31 14:38:49 +0100

    Add card template & script for displaying cards

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ function addBookToLibrary(title, author, pages, read) {
     const myBook = new Book(title, author, pages, read);
     myLibrary.push(myBook);
 }
+
+function createCard(obj) {
+    const template = document.getElementById("#card-template");
+    const card = template.content.cloneNode(true);
+
+    const title = card.querySelector("h3");
+    const author = card.querySelector(".book-author>span");
+    const pages = card.querySelector(".book-pages>span");
+    const read = card.querySelector(".book-read>span");
+
+    title.textContent = obj.title;
+    author.textContent = obj.author;
+    pages.textContent = obj.pages;
+    read.textContent = obj.read ? "Already read" : "Not read yet";
+
+    return card;
+}
+
+function showCards(arr) {
+    const container = document.querySelector(".card-container");
+    arr.forEach((item) => {container.appendChild(item);});
+}
